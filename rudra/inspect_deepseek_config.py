0533eb979db44370ff628e6a6502ac549bfa1cf9
aditya25058 <aditya25058@users.noreply.github.com>
2026-09-19 18:34:07 +0530

    fix moe definition

--- rudra/inspect_deepseek_config.py
+++ rudra/inspect_deepseek_config.py
@@ -27,6 +27,7 @@ with torch.device("meta"):
 print("m.model submodules:", [k for k, _ in meta_model.model.named_children()])
 print("layer 0 submodules:", [k for k, _ in meta_model.model.layers[0].named_children()])
 print("layer 0 self_attn submodules:", [k for k, _ in meta_model.model.layers[0].self_attn.named_children()])
+moe = meta_model.model.layers[1].mlp
 print("moe_infer source:")
 print(inspect.getsource(type(moe).moe_infer))
 
