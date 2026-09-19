4900742a0a1c59fba1937dc8b51b2e30bd0c3b65
aditya25058 <aditya25058@users.noreply.github.com>
2026-09-19 18:33:45 +0530

    getsource of moe_infer

--- rudra/inspect_deepseek_config.py
+++ rudra/inspect_deepseek_config.py
@@ -27,9 +27,9 @@ with torch.device("meta"):
 print("m.model submodules:", [k for k, _ in meta_model.model.named_children()])
 print("layer 0 submodules:", [k for k, _ in meta_model.model.layers[0].named_children()])
 print("layer 0 self_attn submodules:", [k for k, _ in meta_model.model.layers[0].self_attn.named_children()])
-moe = meta_model.model.layers[1].mlp
-print("moe forward source:")
-print(inspect.getsource(type(moe).forward))
+print("moe_infer source:")
+print(inspect.getsource(type(moe).moe_infer))
+
 
 
 
